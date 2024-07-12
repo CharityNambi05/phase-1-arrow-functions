b439dd528e46e9dcc17cc8a91518c52ba7fe162c
CharityNambi05 <charity.nambi@icloud.com>
2024-07-12 21:56:23 +0300

    complete lab

---
+++ index.js
@@ -0,0 +1,21 @@
+
+const divide = function() {
+    return 2000 / 100;
+};
+
+const square = (x) => {
+    
+    return x * x;
+};
+
+
+const add = (a, b) => {
+
+    return a + b;
+};
+
+
+console.log(divide()); // 20
+console.log(square(4)); // 16
+console.log(add(3, 5)); // 8
+
